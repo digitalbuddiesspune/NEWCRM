4c41fbb0124bb553fe5218d7f0d7ddb6cc568252
Prajwal Gamotech <digitalbuddiesspune@gmail.com>
2026-03-15 20:51:40 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,28 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors());
+
+const allowedOrigins = [
+  'https://www.dmcrms.in',
+  'https://dmcrms.in',
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:3000',
+];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin || true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}));
+
 connectDB();
 
 // Define routes
