904672a6fcd3713f11c43739f4675e60abc074a5
Prajwal Gamotech <digitalbuddiesspune@gmail.com>
2026-03-15 22:50:23 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use(
     credentials: true
   })
 );
-app.options('*', cors());
+app.options(/.*/, cors());
 // Body parser
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
